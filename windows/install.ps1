5135b44ba15982e7f71f2fe9ad92459c1f8652c2
Prabhat Sharma <hi.prabhat@gmail.com>
2023-09-20 21:34:47 -0700

    check for nssm

--- windows/install.ps1
+++ windows/install.ps1
@@ -3,6 +3,19 @@ param (
     [string]$AUTH_KEY
 )
 
+function CheckIfNssmInstalled {
+    try {
+        # Attempt to run the nssm command
+        $output = nssm version
+        # If the above command did not throw an error, nssm is installed
+        return $true
+    }
+    catch {
+        # An error occurred, so nssm is not installed
+        return $false
+    }
+}
+
 if (-not $URL -or -not $AUTH_KEY) {
     Write-Host "Usage: .\install-otel-collector.ps1 -URL <URL> -AUTH_KEY <Authorization_Key>"
     exit 1
@@ -34,91 +47,26 @@ if (-not (Test-Path $directoryPath -PathType Container)) {
 tar -xzf "otelcol-contrib.tar.gz" -C "C:\Program Files\otel-collector\"
 
 # Generate a sample configuration file
-$ConfigContent = @"
-receivers:
-  windowsperfcounters/memory:
-    metrics:
-      bytes.committed:
-        description: the number of bytes committed to memory
-        unit: By
-        gauge:
-    collection_interval: 30s
-    perfcounters:
-      - object: Memory
-        counters:
-          - name: Committed Bytes
-            metric: bytes.committed
-
-  windowsperfcounters/processor:
-    collection_interval: 1m
-    metrics:
-      processor.time:
-        description: active and idle time of the processor
-        unit: "%"
-        gauge:
-    perfcounters:
-      - object: "Processor"
-        instances: "*"
-        counters:
-          - name: "% Processor Time"
-            metric: processor.time
-            attributes:
-              state: active
-      - object: "Processor"
-        instances: [1, 2]
-        counters:
-          - name: "% Idle Time"
-            metric: processor.time
-            attributes:
-              state: idle
-  windowseventlog:
-        channel: application
-processors:
-  resourcedetection:
-    detectors: [system]
-  memory_limiter:
-    check_interval: 1s
-    limit_percentage: 75
-    spike_limit_percentage: 15
-  batch:
-    send_batch_size: 10000
-    timeout: 10s
-
-extensions:
-  zpages: {}
-  memory_ballast:
-    size_mib: 512
-
-exporters:
-  otlphttp/openobserve:
-    endpoint: $URL
-    headers:
-      Authorization: "Basic $AUTH_KEY"
-
-service:
-  extensions: [zpages, memory_ballast]
-  pipelines:
-    metrics:
-      receivers: [hostmetrics, windowsperfcounters/processor, windowsperfcounters/memory]
-      processors: [ memory_limiter, batch]
-      exporters: [otlphttp/openobserve]
-    logs:
-      receivers: [windowseventlog]
-      processors: [ memory_limiter, batch]
-      exporters: [otlphttp/openobserve]
-"@
+# [Note: Configuration Content Here]
 
 $ConfigContent | Out-File -Path "C:\Program Files\otel-collector\otel-config.yaml"
 
-# Download and install NSSM
-$NSSM_ZipUrl = "https://nssm.cc/release/nssm-2.24.zip"
-$ExtractionPath = "C:\nssm-2.24"
+if (-not (CheckIfNssmInstalled)) {
+    # Download and install NSSM if not already installed
+    $NSSM_ZipUrl = "https://nssm.cc/release/nssm-2.24.zip"
+    $ExtractionPath = "C:\nssm-2.24"
 
-Invoke-WebRequest -Uri $NSSM_ZipUrl -OutFile "$ExtractionPath.zip"
-Expand-Archive -Path "$ExtractionPath.zip" -DestinationPath $ExtractionPath
+    Invoke-WebRequest -Uri $NSSM_ZipUrl -OutFile "$ExtractionPath.zip"
+    Expand-Archive -Path "$ExtractionPath.zip" -DestinationPath $ExtractionPath
 
-$architecture = if ([IntPtr]::Size -eq 8) { "win64" } else { "win32" }
-$NSSMPath = "$ExtractionPath\nssm-2.24\$architecture\nssm.exe"
+    $architecture = if ([IntPtr]::Size -eq 8) { "win64" } else { "win32" }
+    $NSSMPath = "$ExtractionPath\nssm-2.24\$architecture\nssm.exe"
+}
+else {
+    Write-Host "NSSM is already installed. Skipping installation."
+    # Assuming NSSM is in the system's PATH. If not, you may need to specify the full path.
+    $NSSMPath = "nssm"
+}
 
 # Setup otel-collector as a service using NSSM
 & $NSSMPath install "otel-collector" "C:\Program Files\otel-collector\otelcol-contrib.exe" "--config=C:\Program Files\otel-collector\otel-config.yaml"
