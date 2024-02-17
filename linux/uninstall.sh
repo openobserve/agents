3310f68c39529676c331539792ae93ceb690d2a7
Get your own 'tots. Geez! <YipYup@users.noreply.github.com>
2024-02-16 18:09:14 -0700

    Update uninstall.sh (#5)
    Remove installed binary, config file, and user and group created during install.

--- linux/uninstall.sh
+++ linux/uninstall.sh
@@ -5,4 +5,9 @@ sudo systemctl disable otel-collector
 sudo rm /etc/systemd/system/otel-collector.service
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
+sudo rm -f /usr/local/bin/otelcol-contrib
+sudo rm -f /etc/otel-config.yaml
+sudo userdel --force --remove openobserve-agent
+sudo groupdel openobserve-agent
+
 
